fd61dc655fd1d654a3a00bdd7b4018036b90df99
copyrite <tatu.heikki.leinonen@gmail.com>
2022-03-13 14:41:01 +0200

    Bad rewrite in d3js

--- lw2shotbar.js
+++ lw2shotbar.js
@@ -1,62 +1,28 @@
 window.onload = function() {
-    breakdown = false;
-
     document.querySelectorAll(".input").forEach((elem) => { addEventListener("change", draw) })
-    document.getElementById("breakdown-toggle").addEventListener("click", toggleExplain)
     window.addEventListener("resize", draw)
 
     toggleExplain();
-}
-
-function toggleExplain() {
-    breakdown = !breakdown;
-
-    button = document.getElementById("breakdown-toggle")
-    if (breakdown){
-        document.querySelectorAll(".breakdown").forEach((elem) => { elem.style.display = "inline" });
-        button.innerHTML = "Hide breakdown";
-    }
-    else {
-        document.querySelectorAll(".breakdown").forEach((elem) => { elem.style.display = "none" })
-        button.innerHTML = "Show breakdown";
-    }
-    draw();
 }
 
 function clamp(x){
     return Math.max(0, Math.min(100, x));
 }
 
-function drawBar(canvas, shotbar) {
-    canvas.width = document.body.clientWidth;
-    canvas.height = 10;
-
-    var vis = {width: 0.6*canvas.width, height: canvas.height, left: 0.2*canvas.width};
-
-    var context = canvas.getContext("2d");
-
-    context.fillStyle="#878700";
-    context.fillRect(vis.left, 0, vis.width*shotbar.crit/100, 10);
-
-    context.fillStyle="#870000";
-    context.fillRect(vis.left + vis.width*shotbar.crit/100, 0, vis.width*shotbar.hit/100, 10);
-
-    context.fillStyle="#008700";
-    context.fillRect(vis.left + vis.width*(shotbar.crit + shotbar.hit)/100, 0, vis.width*shotbar.graze/100, 10);
-
-    context.fillStyle="#878787";
-    context.fillRect(vis.left + vis.width*(shotbar.crit + shotbar.hit + shotbar.graze)/100, 0, vis.width*shotbar.miss/100, 10);
-    return canvas;
-}
-
 function draw() {
-    var canvas = document.getElementById("shotbar");
-    canvas.width = document.body.clientWidth;
-    canvas.height = 10;
-    var context = canvas.getContext("2d");
-    context.clearRect(0, 0, canvas.width, canvas.height);
     var breakdownDiv = document.getElementById("breakdown")
     breakdownDiv.innerHTML = ""
+    var widthRatio = 0.6;
+    var dims = {
+        "x": breakdownDiv.getClientRects()[0].width*(1- widthRatio)/2,
+        "width": breakdownDiv.getClientRects()[0].width*widthRatio/100,
+        "height": "1.2em",
+    };
+    d3.select("#breakdown")
+        .append("div")
+        .attr("id", "tooltip")
+        .attr("style", "position: absolute; left: 0; top: 0; transform: translate(-50%, -100%)");
+
 
     // Inputs
     aim = clamp(parseInt(document.getElementById("input-aim").value));
@@ -68,62 +34,72 @@ function draw() {
     shotbars = [];
 
     // Initial
-    initial = {
-        "crit": 0,
-        "hit": aim,
-        "graze": 0,
-        "miss": 100 - aim,
-    };
+    initial = [
+        {"value": 0, color: "#878700", text: "Crit"},
+        {"value": aim, color: "#870000", text: "Normal hit"},
+        {"value": 0, color: "#008700", text: "Graze"},
+        {"value": 100 - aim, color: "#878787", text: "Miss"},
+    ]
+    d3.cumsum([0].concat(initial.slice(0, 3)), elem => elem.value)
+        .forEach((elem, i) => {initial[i].x = elem;});
     shotbars.push(initial);
 
     breakdownDiv.appendChild(document.createElement("p")).innerHTML = "The initial chance to hit is based on Aim:";
-    breakdownDiv.appendChild(drawBar(document.createElement("canvas"), initial));
+
+    d3.select("#breakdown")
+        .append("svg")
+        .attr("width", "100%")
+        .attr("style", `height: ${dims.height}`);
 
 
     // Apply graze band
-    half_bandwidth = Math.min(grazeband, initial.hit, 100-initial.hit);
-    banded = {"crit": 0};
-    banded.hit = initial.hit - half_bandwidth;
-    banded.graze = 2*half_bandwidth;
-    banded.miss = 100 - banded.hit - banded.graze;
+    halfBandwidth = Math.min(grazeband, initial[1].value, 100-initial[1].value);
+    banded = [{"value": 0, color: "#878700", text: "Crit"}];
+    banded.push({"value": initial[1].value - halfBandwidth, color: "#870000", text: "Normal hit"});
+    banded.push({"value": 2*halfBandwidth, color: "#008700", text: "Graze"});
+    banded.push({"value": 100 - banded[0].value - banded[1].value - banded[2].value, color: "#878787", text: "Miss"});
+
+    d3.cumsum([0].concat(banded.slice(0, 3)), elem => elem.value)
+        .forEach((elem, i) => {banded[i].x = elem;})
     shotbars.push(banded);
+
     if (grazeband > 0) {
-        if (initial.hit == 0) {
+        if (initial[1].value == 0) {
             breakdownDiv.appendChild(document.createElement("p")).innerHTML = "Graze band would be applied here, but zero Aim has eliminated it completely."
         }
-        else if (initial.miss == 0) {
+        else if (initial[3].value == 0) {
             breakdownDiv.appendChild(document.createElement("p")).innerHTML = "Graze band would be applied here, but zero miss chance has eliminated it completely."
         }
         else {
 
-            if (initial.hit < grazeband){
+            if (initial[1].value < grazeband){
                 helpText = "The graze band is applied equally on both sides around Aim. Because the chance to hit is low, the size of graze band is only equal to Aim:";
             }
-            else if (100 - initial.hit < grazeband){
+            else if (100 - initial[1].value < grazeband){
                 helpText = "The graze band is applied equally on both sides around Aim. Because the chance to miss is low, the size of graze band is only equal to miss chance:";
             }
             else {
                 helpText = "The graze band is applied equally on both sides around Aim:";
             }
             breakdownDiv.appendChild(document.createElement("p")).innerHTML = helpText;
-
-        }
-
-        if (initial.hit != banded.hit || initial.miss != banded.miss){
-            breakdownDiv.appendChild(drawBar(document.createElement("canvas"), banded));
         }
     }
 
+    d3.select("#breakdown")
+        .append("svg")
+        .attr("width", "100%")
+        .attr("style", `height: ${dims.height}`);
 
     // Promotion
     promote = Math.min(crit - Math.min(0, dodge), 100); // Negative dodge is additional crit
-    promoted = {
-        "crit": promote*banded.hit/100,
-        "hit": banded.hit + promote*(banded.graze - banded.hit)/100,
-        "graze": banded.graze - promote*banded.graze/100,
-    }
-    promoted.miss = 100 - promoted.crit - promoted.hit - promoted.graze
-    shotbars.push(promoted)
+    promoted = [{"value": promote*banded[1].value/100, color: "#878700", text: "Crit"}];
+    promoted.push({"value": banded[1].value + promote*(banded[2].value - banded[1].value)/100, color: "#870000", text: "Normal hit"});
+    promoted.push({"value": banded[2].value - promote*banded[2].value/100, color: "#008700", text: "Graze"});
+    promoted.push({"value": 100 - promoted[0].value - promoted[1].value - promoted[2].value, color: "#878787", text: "Miss"});
+
+    d3.cumsum([0].concat(promoted.slice(0, 3)), elem => elem.value)
+        .forEach((elem, i) => {promoted[i].x = elem;});
+    shotbars.push(promoted);
 
     if (promote == 0){
         breakdownDiv.appendChild(document.createElement("p")).innerHTML = "Hit promotion would be applied here, but Crit is zero and Dodge is not negative."
@@ -138,17 +114,21 @@ function draw() {
         else {
             breakdownDiv.appendChild(document.createElement("p")).innerHTML = "Crit and negative Dodge are added together, promoting normal hits into crits and grazes into normal hits."
         }
-        breakdownDiv.appendChild(drawBar(document.createElement("canvas"), promoted));
     }
+
+    d3.select("#breakdown")
+        .append("svg")
+        .attr("width", "100%")
+        .attr("style", `height: ${dims.height}`)
 
     // Demotion
     demote = Math.min(Math.max(dodge, 0), 100);
-    demoted = {
-        "crit": (100 - demote)*promoted.crit/100,
-        "hit": promoted.hit + demote*(promoted.crit - promoted.hit)/100,
-        "graze": promoted.graze + demote*(promoted.hit - promoted.graze)/100,
-    }
-    demoted.miss = 100 - demoted.crit - demoted.hit - demoted.graze
+    demoted = [{"value": (100 - demote)*promoted[0].value/100, color: "#878700", text: "Crit"}]
+    demoted.push({"value": promoted[1].value + demote*(promoted[0].value - promoted[1].value)/100, color: "#870000", text: "Normal hit"})
+    demoted.push({"value": promoted[2].value + demote*(promoted[1].value - promoted[2].value)/100, color: "#008700", text: "Graze"})
+    demoted.push({"value": 100 - demoted[0].value - demoted[1].value - demoted[2].value, color: "#878787", text: "Miss"})
+    d3.cumsum([0].concat(demoted.slice(0, 3)), elem => elem.value)
+        .forEach((elem, i) => {demoted[i].x = elem;})
     shotbars.push(demoted)
 
     if (demote == 0){
@@ -156,18 +136,46 @@ function draw() {
     }
     else {
         breakdownDiv.appendChild(document.createElement("p")).innerHTML = "Dodge demotes Crits into normal hits, normal hits into grazes, and grazes into misses.";
-        breakdownDiv.appendChild(drawBar(document.createElement("canvas"), demoted));
     }
+
+    d3.select("#breakdown")
+        .append("svg")
+        .attr("width", "100%")
+        .attr("style", `height: ${dims.height}`)
 
     lastBar = shotbars[shotbars.length-1]
     // Textual output
     internalNames = ["crit", "hit", "graze", "miss"];
     userNames = ["Crit", "Normal hit", "Graze", "Miss"];
     for (i = 0; i < 4; ++i){
-        document.getElementById("output-" + internalNames[i]).innerHTML = userNames[i] + ": " + (lastBar[internalNames[i]]).toFixed(3) + "%"
+        document.getElementById("output-" + internalNames[i]).innerHTML = userNames[i] + ": " + (lastBar[i].value).toFixed(3) + "%"
     }
 
 
-    // Render final shotbar
-    drawBar(document.getElementById("shotbar"), lastBar);
+    d3.selectAll("svg")
+        .data(shotbars)
+        .join("svg")
+        .selectAll("rect")
+        .data(d => d)
+        .join("rect")
+        .each(elem => {elem.text = `${elem.text}: ${elem.value.toFixed(3)}%`})
+        .attr("x", (elem) => dims.x + elem.x*dims.width)
+        .attr("y", (elem) => 0)
+        .attr("width", (elem) => elem.value*dims.width)
+        .attr("height", (elem) => dims.height)
+        .attr("fill", (elem) => elem.color)
+        .attr("rx", (elem) => 2)
+        .attr("ry", (elem) => 2)
+        .attr("text", (elem) => elem.text)
+        .on("mouseleave", (event) => {
+            d3.select("#tooltip")
+                .style("display", "none")
+        })
+        .on("mouseover", (event) => {
+            d3.select("#tooltip")
+                .style("display", "block")
+                .style("left", event.target.getScreenCTM().e + window.pageXOffset + event.target.x.animVal.value + event.target.width.animVal.value/2 + "px")
+                .style("top", event.target.getScreenCTM().f + window.pageYOffset + event.target.y.animVal.value + "px")
+                .text(event.target.getAttribute("text"));
+        });
 };
